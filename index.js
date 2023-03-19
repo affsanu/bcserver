562ce108df607b26739bd8fe8aea9dd085fb9725
Sanu Islam <124078469+affsanu@users.noreply.github.com>
2023-03-19 21:31:20 +0600

    add cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const userRouter = require('./routes/userRoute');
 const port = process.env.PORT || 8003
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: false}));
+app.use(cors());
 
 
 mongoose.set('strictQuery', false);
