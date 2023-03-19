27609aeaaa901f1a916c57a8ec1636c7d1070616
spluscsdp <aff.sanu@gmail.com>
2023-03-19 20:12:06 +0600

    adding files

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const CreateOrder = require('../controllers/orderController');
 const { Register, Login, GetAllUsers, GetSingleUser } = require('../controllers/userController');
 const router = express.Router();
 
@@ -8,6 +7,6 @@ router.post('/login', Login);
 router.get('/allusers', GetAllUsers);
 router.get('/:id', GetSingleUser);
 
-router.post('/order', CreateOrder);
+
 
 module.exports = router;
